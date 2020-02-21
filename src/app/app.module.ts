19f4fa686292cca80b5c355a83f7a674ccb9c703
Guibson Krause <guibsonkrause@localhost.localdomain>
2020-02-21 00:24:48 -0300

    Adicionado Módulo Core

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,45 +1,22 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { TableModule } from 'primeng/table';
-import {ButtonModule} from 'primeng/button';
-import { TooltipModule } from 'primeng/tooltip';
-import { NavbarComponent } from './navbar/navbar.component';
-import {InputTextModule} from 'primeng/inputtext';
-import { CalendarModule } from 'primeng/calendar';
-import {CheckboxModule, ChipsModule, DropdownModule, InputMaskModule, ListboxModule, PaginatorModule} from 'primeng';
-import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
-import {FormsModule} from '@angular/forms';
 import {AlunosModule} from './alunos/alunos.module';
 import {ProfessoresModule} from './professores/professores.module';
 import {DisciplinasModule} from './disciplinas/disciplinas.module';
+import {CoreModule} from './core/core.module';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent,
   ],
   imports: [
     BrowserModule,
-    FormsModule,
-    AppRoutingModule,
-    TableModule,
-    ButtonModule,
-    TooltipModule,
-    ChipsModule,
-    InputTextModule,
-    CalendarModule,
-    BrowserAnimationsModule,
-    DropdownModule,
-    InputMaskModule,
-    PaginatorModule,
-    ListboxModule,
-    CheckboxModule,
     AlunosModule,
     ProfessoresModule,
     DisciplinasModule,
+    CoreModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
