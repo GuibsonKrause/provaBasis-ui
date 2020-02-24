2276f7087d5e6173c11d50f6ec587a762d95506d
Guibson Krause <guibsonkrause@localhost.localdomain>
2020-02-24 14:49:09 -0300

    Preenchendo list de disciplinas no cadastro de alunos

--- src/app/alunos/aluno-cadastro/aluno-cadastro.component.ts
+++ src/app/alunos/aluno-cadastro/aluno-cadastro.component.ts
@@ -2,19 +2,13 @@ import {Component, OnInit} from '@angular/core';
 import {DisciplinaService} from '../../disciplinas/disciplina.service';
 import {ErrorHandlerService} from '../../core/error-handler.service';
 
-interface Disciplina {
-  name: any;
-  code: any;
-}
-
 @Component({
   selector: 'app-aluno-cadastro',
   templateUrl: './aluno-cadastro.component.html',
   styleUrls: ['./aluno-cadastro.component.css']
 })
 export class AlunoCadastroComponent implements OnInit {
-  disciplinas: Disciplina[];
-  disciplinasSelecionadas: Disciplina[];
+  disciplinas = [];
 
   constructor(
     private disciplinaService: DisciplinaService,
@@ -28,7 +22,7 @@ export class AlunoCadastroComponent implements OnInit {
   carregarDisciplinas() {
     this.disciplinaService.pesquisar()
       .then(disciplinas => {
-        this.disciplinas = disciplinas.map(d => ({ nome: d.nome, code: d.id }));
+        this.disciplinas = disciplinas.map(d => ({ name: d.nome, code: d.id }));
       })
       .catch(erro => this.errorHandler.handler(erro));
   }
